ea6b7335edecf3f2c55bf6c2fd10fe30ee8ea1d6
bcwalker22 <walkerb2022@fau.edu>
2026-04-15 21:05:18 -0400

    add client

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './CreatePost.css'
+import { supabase } from '../client'
 
 const CreatePost = () => {
 
@@ -7,12 +8,27 @@ const CreatePost = () => {
 
     const handleChange = (event) => {
         const {name, value} = event.target
-        setPost( (prev) => {
+        setPost((prev) => {
             return {
                 ...prev,
-                [name]:value,
+                [name]: value,
             }
         })
+    }
+
+    const createPost = async (event) => {
+        event.preventDefault()
+
+        await supabase
+            .from('Posts')
+            .insert({
+                title: post.title,
+                author: post.author,
+                description: post.description
+            })
+            .select()
+
+        window.location = "/"
     }
 
     return (
@@ -30,7 +46,7 @@ const CreatePost = () => {
                 <textarea rows="5" cols="50" id="description" name="description" onChange={handleChange}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={createPost} />
             </form>
         </div>
     )
