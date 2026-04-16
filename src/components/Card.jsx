5f166d60f200d961f7b38857ec5830aef28037e1
bcwalker22 <walkerb2022@fau.edu>
2026-04-15 21:23:21 -0400

    modify card ui sync

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,36 @@ import { useState } from 'react'
 import './Card.css'
 import more from './more.png'
 import { Link } from 'react-router-dom'
-
+import { supabase } from '../client'
 
 const Card = (props) =>  {
 
-  const [count, setCount] = useState(0)
-  const updateCount = () => {
+  const [count, setCount] = useState(props.betCount)
+
+  const updateCount = async (event) => {
+    event.preventDefault()
+
+    await supabase
+      .from('Posts')
+      .update({ betCount: count + 1 })
+      .eq('id', props.id)
+
     setCount((count) => count + 1)
   }
 
   return (
       <div className="Card">
-          <Link to={'edit/'+ props.id}><img className="moreButton" alt="edit button" src={more} /></Link>
+          <Link to={'edit/'+ props.id}>
+            <img className="moreButton" alt="edit button" src={more} />
+          </Link>
+
           <h2 className="title">{props.title}</h2>
           <h3 className="author">{"by " + props.author}</h3>
           <p className="description">{props.description}</p>
-          <button className="betButton" onClick={updateCount} >👍 Bet Count: {count}</button>
+
+          <button className="betButton" onClick={updateCount}>
+            👍 Bet Count: {count}
+          </button>
       </div>
   );
 };
